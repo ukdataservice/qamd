c60cfa826ef9f65de21b5537f5af6e9646043a1d
Myles Offord <mylesofford@hotmail.com>
2018-07-04 14:55:58 +0100

    bootstrap css for html output

--- src/html.rs
+++ src/html.rs
@@ -11,42 +11,42 @@ pub fn to_html(report: &Report) -> String {
             head {
                 title : &report.metadata.file_name;
                 meta(charset="UTF-8");
-                style : r#"
-                    table, th, td {
-                        border: 1px solid black;
-                        padding: 4px;
-                    }
-                    .passed {
-                        background-color: rgb(212, 237, 218);;
-                    }
-                    .failed {
-                        background-color: rgb(248, 215, 218);;
-                    }
-                    "#;
+                link(rel="stylesheet",
+                     href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css",
+                     integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB",
+                     crossorigin="anonymous");
             }
             body {
-                h1(id="file-name", class="file-name") : &report.metadata.file_name;
-
-                table {
-                    tr {
-                        th { p { : "Name" } }
-                        th { p { : "Status" } }
-                        th { p { : "Description" } }
+                div(class="container") {
+                    div(class="row") {
+                        h1(id="file-name", class="file-name") : &report.metadata.file_name;
                     }
 
-                    @ for check in report.summary.clone() {
-                        @ if let (name, Some(status)) = check {
-                            @ if status.fail > 0 {
-                                tr(class="failed") {
-                                    td { : format!("{}", name) }
-                                    td : format!("failed ({})", status.fail);
-                                    td : &status.desc;
-                                }
-                            } else {
-                                tr(class="passed") {
-                                    td { : format!("{}", name) }
-                                    td : "passed";
-                                    td : &status.desc;
+                    br;
+
+                    div(class="row") {
+                        table(class="table table-bordered") {
+                            tr {
+                                th(scope="col") : "Name";
+                                th(scope="col") : "Status";
+                                th(scope="col") : "Description";
+                            }
+
+                            @ for check in report.summary.clone() {
+                                @ if let (name, Some(status)) = check {
+                                    @ if status.fail > 0 {
+                                        tr(class="table-danger") {
+                                            td(scope="row") : format!("{}", name);
+                                            td : format!("failed ({})", status.fail);
+                                            td : &status.desc;
+                                        }
+                                    } else {
+                                        tr(class="table-success") {
+                                            td(scope="row") : format!("{}", name);
+                                            td : "passed";
+                                            td : &status.desc;
+                                        }
+                                    }
                                 }
                             }
                         }
